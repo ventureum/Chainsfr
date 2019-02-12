68079db846db42b410ce2a436f7594bea54a1afa
Timothy Wang <timothywangdev@gmail.com>
2019-02-12 07:01:44 +0000

    Resolve #70 Add ReceiveContainer

---
+++ src/containers/ReceiveContainer.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import ReceiveComponent from '../components/ReceiveComponent'
+
+import paths from '../Paths'
+
+const path2Step = {
+  [paths.receivePasswordStep]: 0,
+  [paths.receiveWalletSelectionStep]: 1,
+  [paths.receiveReviewStep]: 1
+}
+
+class ReceiveContainer extends Component {
+  render () {
+    let stepParam = this.props.match.params.step
+
+    return (
+      <ReceiveComponent step={path2Step['/' + stepParam]} history={this.props.history} />
+    )
+  }
+}
+
+export default ReceiveContainer
