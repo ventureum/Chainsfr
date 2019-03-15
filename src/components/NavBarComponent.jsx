7c2bcc929a7ddc526f556951793f945f1e070e8d
clzhong <vincentzhongca@gmail.com>
2019-03-15 10:47:14 -0400

    Resolve #213 Click logo returns to home page

--- src/components/NavBarComponent.jsx
+++ src/components/NavBarComponent.jsx
@@ -5,6 +5,9 @@ import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import AccountCircle from '@material-ui/icons/AccountCircle'
 import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
+import { Link } from 'react-router-dom'
+import path from '../Paths.js'
 
 class NavBarComponent extends Component {
   render () {
@@ -12,9 +15,11 @@ class NavBarComponent extends Component {
     return (
       <AppBar position='static'>
         <Toolbar>
-          <Typography variant='h6' color='inherit' className={classes.grow}>
-            Chainsfer
-          </Typography>
+          <Button classes={{ root: classes.button }} component={Link} to={path.home} >
+            <Typography variant='h6' color='inherit' className={classes.grow}>
+              Chainsfer
+            </Typography>
+          </Button>
           <AccountCircle />
         </Toolbar>
       </AppBar>
@@ -25,6 +30,11 @@ class NavBarComponent extends Component {
 const styles = theme => ({
   grow: {
     flexGrow: 1
+  },
+  button: {
+    '&:hover': {
+      backgroundColor: 'transparent'
+    }
   }
 })
 
