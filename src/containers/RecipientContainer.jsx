7b75ec06b5258e251b129f062a3c96507b6506b2
Timothy Wang <timothywangdev@gmail.com>
2019-03-20 15:34:14 +0000

    Resp;ve #259 Login user sender address is not prefilled

--- src/containers/RecipientContainer.jsx
+++ src/containers/RecipientContainer.jsx
@@ -3,6 +3,7 @@ import { connect } from 'react-redux'
 import Recipient from '../components/RecipientComponent'
 import { updateTransferForm, generateSecurityAnswer, clearSecurityAnswer } from '../actions/formActions'
 import { goToStep } from '../actions/navigationActions'
+import update from 'immutability-helper'
 
 type Props = {
   updateTransferForm: Function,
@@ -15,6 +16,15 @@ type Props = {
 }
 
 class RecipientContainer extends Component<Props> {
+  componentDidMount () {
+    let { profile, transferForm, updateTransferForm } = this.props
+
+    if (profile.isAuthenticated) {
+      // prefill sender's email address for authenticated user
+      updateTransferForm(update(transferForm, { sender: { $set: profile.profileObj.email } }))
+    }
+  }
+
   render () {
     return (
       <Recipient
@@ -37,7 +47,8 @@ const mapStateToProps = state => {
   return {
     cryptoSelection: state.formReducer.cryptoSelection,
     transferForm: state.formReducer.transferForm,
-    wallet: state.walletReducer.wallet[state.formReducer.walletSelection]
+    wallet: state.walletReducer.wallet[state.formReducer.walletSelection],
+    profile: state.userReducer.profile
   }
 }
 
