8c7a46fd17d0e222c0e5f9e4118b02c8694acede
clzhong <vincentzhongca@gmail.com>
2019-03-28 17:06:02 -0400

    Resolve #306 Add cloudWallet balance on /transfer
    - Add cloud wallet balance section on /transfer

--- src/containers/LandingPageContainer.jsx
+++ src/containers/LandingPageContainer.jsx
@@ -5,10 +5,12 @@ import LandingPageComponent from '../components/LandingPageComponent'
 import { goToStep } from '../actions/navigationActions'
 import { getTransferHistory } from '../actions/transferActions'
 import { createLoadingSelector, createErrorSelector } from '../selectors'
+import { getCloudWallet } from '../actions/walletActions'
 
 class LandingPageContainer extends Component {
   componentDidMount () {
     if (this.props.profile.isAuthenticated) {
+      if (!this.props.cloudWallet.connected) this.props.getCloudWallet()
       // load transfer history for logged-in users
       this.props.getTransferHistory()
     }
@@ -24,15 +26,19 @@ class LandingPageContainer extends Component {
 }
 
 const getTransferHistorySelector = createLoadingSelector(['GET_TRANSFER_HISTORY'])
-const errorSelector = createErrorSelector(['GET_TRANSFER_HISTORY'])
+const errorSelector = createErrorSelector(['GET_TRANSFER_HISTORY', 'GET_CLOUD_WALLET'])
+const getCloudWalletSelector = createLoadingSelector(['GET_CLOUD_WALLET'])
 
 const mapStateToProps = state => {
   return {
     transferHistory: state.transferReducer.transferHistory,
     profile: state.userReducer.profile,
+    cloudWalletConnected: state.walletReducer.wallet.cloudWallet.connected,
     actionsPending: {
-      getTransferHistory: getTransferHistorySelector(state)
+      getTransferHistory: getTransferHistorySelector(state),
+      getCloudWallet: getCloudWalletSelector(state)
     },
+    cloudWallet: state.walletReducer.wallet.cloudWallet,
     error: errorSelector(state)
   }
 }
@@ -40,6 +46,7 @@ const mapStateToProps = state => {
 const mapDispatchToProps = dispatch => {
   return {
     goToStep: (n) => dispatch(goToStep('send', n)),
+    getCloudWallet: () => dispatch(getCloudWallet()),
     getTransferHistory: () => dispatch(getTransferHistory())
   }
 }
