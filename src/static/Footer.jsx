e27848381f463f6e8892ece20ac3e0b2062783b8
clzhong <vincentzhongca@gmail.com>
2019-07-18 14:35:24 -0400

    Resolve #664 Improve Chainsfr footer

--- src/static/Footer.jsx
+++ src/static/Footer.jsx
@@ -8,16 +8,73 @@ class FooterComponent extends React.Component {
     const { classes } = this.props
     return (
       <footer className={classes.root}>
-        <Grid container direction='column' alignItems='center' justify='center' className={classes.copyRightContainer}>
-          <Grid item >
-            <Typography className={classes.copyright}>
-            &copy; {'2018 - '}{1900 + new Date().getYear()} All rights reserved.
-            </Typography>
+        <div className={classes.sectionContainer}>
+          <Grid container className={classes.linkContainer} direction='row'>
+            <Grid item xs={12} sm={3} className={classes.itemContainer}>
+              <Grid container justify='center'>
+                <Typography className={classes.chainsfrText}>
+                  chainsfr
+                </Typography>
+              </Grid>
+            </Grid>
+            <Grid item xs={12} sm={5} className={classes.itemContainer}>
+              <Typography className={classes.descriptionText}>
+                Chainsfr is a cryptocurrency transfer service, powered by Google Drive APIs and Amazon Web Services.
+              </Typography>
+            </Grid>
+            <Grid item xs={12} sm={4} className={classes.itemContainer}>
+              <Grid container justify='center' alignItems='center'>
+                <Grid item>
+                  <a
+                    href='https://docs.google.com/document/d/e/2PACX-1vScWE32Rzf-z8OYmzS9mKTBcpVftWMbeR_BfbhmeJxHDF4jaiYXyUfPOtGif7mI6RSpoQ19onaawdYE/pub'
+                    target='_blank'
+                    rel='noopener noreferrer'
+                    className={classes.link}
+                  >
+                    Terms
+                  </a>
+                </Grid>
+                <Grid item>
+                  <a
+                    target='_blank'
+                    rel='noopener noreferrer'
+                    href='https://chat.chainsfr.com'
+                    className={classes.link}>
+                    <i className='fab fa-rocketchat' />
+                  </a>
+                </Grid>
+                <Grid item>
+                  <a
+                    rel='noopener noreferrer'
+                    href='https://twitter.com/chainsfr_com'
+                    target='_blank'
+                    className={classes.link}>
+                    <i className='fab fa-twitter' />
+                  </a>
+                </Grid>
+                <Grid item>
+                  <a
+                    target='_blank'
+                    rel='noopener noreferrer'
+                    href='https://github.com/ventureum/Chainsfr'
+                    className={classes.link}>
+                    <i className='fab fa-github' />
+                  </a>
+                </Grid>
+              </Grid>
+            </Grid>
           </Grid>
-          <Typography className={classes.copyright}>
-            Build {process.env.REACT_APP_VERSION}-{process.env.REACT_APP_ENV}
-          </Typography>
-        </Grid>
+          <Grid container direction='column' alignItems='center' justify='center' className={classes.copyRightContainer}>
+            <Typography className={classes.copyright}>
+              Build {process.env.REACT_APP_VERSION}-{process.env.REACT_APP_ENV}
+            </Typography>
+            <Grid item >
+              <Typography className={classes.copyright}>
+                &copy; {'2018 - '}{1900 + new Date().getYear()} All rights reserved.
+              </Typography>
+            </Grid>
+          </Grid>
+        </div>
       </footer>
     )
   }
@@ -26,16 +83,50 @@ class FooterComponent extends React.Component {
 const style = theme => ({
   root: {
     backgroundColor: '#333333',
-    height: '60px'
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center'
+  },
+  sectionContainer: {
+    width: '100%',
+    minHeight: '130px',
+    maxWidth: '1200px',
+    padding: '50px 0px 20px 0px',
+    margin: '0px 20px 0px 20px',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'space-between'
   },
   copyright: {
     fontSize: '12px',
     letterSpacing: '0.2px',
     lineHeight: '18px',
-    color: '#a8a8a8'
+    color: '#a8a8a8',
+    marginTop: '5px'
   },
   copyRightContainer: {
-    height: '100%'
+    height: '100%',
+    marginTop: '30px'
+  },
+  link: {
+    color: '#fff',
+    textDecoration: 'none',
+    marginRight: '20px'
+  },
+  descriptionText: {
+    fontWeight: '400',
+    fontSize: '16px',
+    color: '#fff',
+    margin: '0px 10px 0px 10px'
+  },
+  chainsfrText: {
+    fontSize: '28px',
+    fontWeight: '800',
+    color: '#fff'
+  },
+  itemContainer: {
+    marginTop: '20px'
   }
 })
 
