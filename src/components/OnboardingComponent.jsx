e589bba751125870dca2bc9ab4a180b1f1300bf8
clzhong <vincentzhongca@gmail.com>
2019-10-01 15:56:39 -0400

    Resolve #837 Onboarding password text field length mismatch

--- src/components/OnboardingComponent.jsx
+++ src/components/OnboardingComponent.jsx
@@ -66,131 +66,109 @@ class OnboardingComponent extends Component {
     let { classes, actionsPending, profile } = this.props
     const onboardingActionsPending = actionsPending.createCloudWallet || actionsPending.register
     return (
-      <div className={classes.root}>
-        <Grid container direction='column' alignItems='center'>
-          <Grid container direction='column' className={classes.container} alignItems='center'>
-            {/* Center the entire container, this step is necessary to make upper and lower section to have same width */}
+      <Grid container direction='column' alignItems='center'>
+        {/* Center the entire container, this step is necessary to make upper and lower section to have same width */}
+        <Grid item className={classes.container}>
+          <Grid container direction='column' justify='center' alignItems='stretch' spacing={3}>
             <Grid item>
-              <Grid container direction='column' justify='center' alignItems='stretch' spacing={3}>
-                <Grid item>
-                  <Typography variant='h2' align='left'>
-                    Welcome to Chainsfr,
-                  </Typography>
-                  <Typography variant='h2' align='left'>
-                    {profile.profileObj.name}
-                  </Typography>
-                </Grid>
-                <Grid item>
-                  <Typography
-                    variant='h4'
-                    style={{
-                      maxWidth: '340px'
-                    }}
-                    align='left'
-                  >
-                    We are going to create a Drive wallet for you. Please set an independent
-                    password to protect your Drive wallet and your future transfers.
-                  </Typography>
-                </Grid>
-                <Grid item>
-                  <TextField
-                    id='outlined-adornment-password'
-                    variant='outlined'
-                    fullWidth
-                    type={'password'}
-                    label='Password'
-                    value={this.state.password}
-                    onChange={this.handleChange('password')}
-                    disabled={onboardingActionsPending}
-                  />
-                </Grid>
-                <Grid item>
-                  <TextField
-                    id='outlined-adornment-password-confirmation'
-                    variant='outlined'
-                    fullWidth
-                    type={'password'}
-                    label='Confirm Password'
-                    value={this.state.passwordConfirmation}
-                    onChange={this.handleChange('passwordConfirmation')}
-                    error={!this.isPasswordMatched()}
-                    helperText={!this.isPasswordMatched() && 'Passwords must match'}
-                    disabled={onboardingActionsPending}
-                  />
-                </Grid>
-                <Grid item>
-                  <FormControlLabel
-                    control={
-                      <Checkbox
-                        checked={this.state.termsAccepted}
-                        onChange={this.handleCheckboxChange}
-                        color='primary'
-                        disabled={onboardingActionsPending}
-                      />
-                    }
-                    label={this.renderTermsAndConditionsLabel()}
-                  />
-                </Grid>
-                {actionsPending.createCloudWallet && (
-                  <Grid item>
-                    <Grid
-                      container
-                      direction='column'
-                      justify='center'
-                      className={classes.loadingSection}
-                    >
-                      <Grid item>
-                        <Typography variant='h4'>This may take up to one minute.</Typography>
-                        <Typography variant='caption'>
-                          {CREATE_WALLET_PROGRESS[this.state.step]}
-                        </Typography>
-                        <LinearProgress className={classes.linearProgress} />
-                      </Grid>
-                    </Grid>
-                  </Grid>
-                )}
-                <Grid item align='center'>
-                  <Button
-                    variant='contained'
+              <Typography variant='h2' align='left'>
+                Welcome to Chainsfr,
+              </Typography>
+              <Typography variant='h2' align='left'>
+                {profile.profileObj.name}
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Typography variant='h4' align='left'>
+                We are going to create a Drive wallet for you. Please set an independent password to
+                protect your Drive wallet and your future transfers.
+              </Typography>
+            </Grid>
+            <Grid item style={{ width: '100%' }}>
+              <TextField
+                id='outlined-adornment-password'
+                variant='outlined'
+                fullWidth
+                type={'password'}
+                label='Password'
+                value={this.state.password}
+                onChange={this.handleChange('password')}
+                disabled={onboardingActionsPending}
+              />
+            </Grid>
+            <Grid item style={{ width: '100%' }}>
+              <TextField
+                id='outlined-adornment-password-confirmation'
+                variant='outlined'
+                fullWidth
+                type={'password'}
+                label='Confirm Password'
+                value={this.state.passwordConfirmation}
+                onChange={this.handleChange('passwordConfirmation')}
+                error={!this.isPasswordMatched()}
+                helperText={!this.isPasswordMatched() && 'Passwords must match'}
+                disabled={onboardingActionsPending}
+              />
+            </Grid>
+            <Grid item>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={this.state.termsAccepted}
+                    onChange={this.handleCheckboxChange}
                     color='primary'
-                    disabled={
-                      !this.state.termsAccepted ||
-                      !this.isPasswordMatched() ||
-                      this.state.password === '' ||
-                      onboardingActionsPending
-                    }
-                    onClick={this.onSubmit}
-                  >
-                    Start Using Chainsfr
-                  </Button>
+                    disabled={onboardingActionsPending}
+                  />
+                }
+                label={this.renderTermsAndConditionsLabel()}
+              />
+            </Grid>
+            {actionsPending.createCloudWallet && (
+              <Grid item>
+                <Grid
+                  container
+                  direction='column'
+                  justify='center'
+                  className={classes.loadingSection}
+                >
+                  <Grid item>
+                    <Typography variant='h4'>This may take up to one minute.</Typography>
+                    <Typography variant='caption'>
+                      {CREATE_WALLET_PROGRESS[this.state.step]}
+                    </Typography>
+                    <LinearProgress className={classes.linearProgress} />
+                  </Grid>
                 </Grid>
               </Grid>
+            )}
+            <Grid item align='center'>
+              <Button
+                variant='contained'
+                color='primary'
+                disabled={
+                  !this.state.termsAccepted ||
+                  !this.isPasswordMatched() ||
+                  this.state.password === '' ||
+                  onboardingActionsPending
+                }
+                onClick={this.onSubmit}
+              >
+                Start Using Chainsfr
+              </Button>
             </Grid>
           </Grid>
         </Grid>
-      </div>
+      </Grid>
     )
   }
 }
 
 const styles = theme => ({
-  root: {
-    flex: 1
-  },
   container: {
     marginTop: '60px',
-    '@media (min-width: 380px) and (max-width : 751px)': {
-      maxWidth: '380px'
-    },
-    '@media (min-width: 752px) and (max-width : 1129px)': {
-      maxWidth: '752px'
-    },
-    '@media (min-width: 1130px) and (max-width : 1489px)': {
-      maxWidth: '1130px'
-    },
-    '@media (min-width: 1490px) ': {
-      maxWidth: '1490px'
-    }
+    width: '100%',
+    maxWidth: '390px',
+    padding: '10px'
   },
   wrapper: {
     margin: theme.spacing(1),
