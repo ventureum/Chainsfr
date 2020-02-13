cfbb091b545602828ecd031d1aad1c499dfac007
clzhong <vincentzhongca@gmail.com>
2020-02-13 14:55:43 -0500

    Resolve #1148 Navigation bar change to left navigation drawer

--- src/containers/AppBarContainer.jsx
+++ src/containers/AppBarContainer.jsx
@@ -1,12 +1,22 @@
+// @flow
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import NavBarComponent from '../components/NavBarComponent'
+import AppBarComponent from '../components/AppBarComponent'
 import { onLogout } from '../actions/userActions'
 import queryString from 'query-string'
 import { backToHome } from '../actions/navigationActions'
 import path from '../Paths.js'
 
-class NavBarContainer extends Component {
+type Props = {
+  onLogout: Function,
+  profile: Object,
+  location: Object,
+  cloudWalletConnected: Boolean,
+  handleDrawerToggle: Function,
+  backToHome: Function
+}
+
+class AppBarContainer extends Component<Props> {
   backToHome = () => {
     const { location, backToHome } = this.props
     if (location.pathname === path.transfer) {
@@ -15,17 +25,28 @@ class NavBarContainer extends Component {
   }
 
   render () {
-    let { onLogout, profile, location, cloudWalletConnected } = this.props
+    let { onLogout, profile, location, cloudWalletConnected, handleDrawerToggle } = this.props
     const urlParams = queryString.parse(location.search)
     let step = parseInt(urlParams.step) || 0
+    const navigatable = ![
+      path.transfer,
+      path.cancel,
+      path.receipt,
+      path.receive,
+      path.login,
+      path.directTransfer
+    ].includes(location.pathname)
     return (
-      <NavBarComponent
+      <AppBarComponent
         onLogout={onLogout}
         profile={profile}
         cloudWalletConnected={cloudWalletConnected}
         backToHome={this.backToHome}
         location={location}
         step={step}
+        handleDrawerToggle={handleDrawerToggle}
+        navigatable={navigatable}
+        disabled={!navigatable}
       />
     )
   }
@@ -45,4 +66,4 @@ const mapStateToProps = state => {
   }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(NavBarContainer)
+export default connect(mapStateToProps, mapDispatchToProps)(AppBarContainer)
