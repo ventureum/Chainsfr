ed7d4bc64083f5ee049ed8ee556b7731980b2964
clzhong <vincentzhongca@gmail.com>
2020-03-20 16:40:08 -0400

    Resolve #523 [Test] LandingPage

---
+++ src/tests/e2e/pages/landing.page.js
@@ -0,0 +1,111 @@
+import { sleep, runUntilEvaluateEquals, getNewPopupPage } from '../testUtils'
+
+class LandingPage {
+  async startEmailTransfer () {
+    await Promise.all([page.waitForNavigation(), page.click('[data-test-id="emt_btn"]')])
+    expect(page.url()).toEqual(`${process.env.E2E_TEST_URL}/send`)
+  }
+
+  async navigateToOverview () {
+    if (page.url() === `${process.env.E2E_TEST_URL}`) {
+      await page.click('#Overview-NavBtn')
+      expect(page.url()).toEqual(`${process.env.E2E_TEST_URL}`)
+    } else {
+      await Promise.all([page.waitForNavigation(), page.click('[data-test-id="overview_nav_btn"]')])
+      expect(page.url()).toEqual(`${process.env.E2E_TEST_URL}/`)
+    }
+  }
+
+  async navigateToAccounts () {
+    await Promise.all([page.waitForNavigation(), page.click('[data-test-id="cya_btn"]')])
+    expect(page.url()).toEqual(`${process.env.E2E_TEST_URL}/accounts`)
+
+    await Promise.all([page.waitForNavigation(), page.click('[data-test-id="accounts_nav_btn"]')])
+    expect(page.url()).toEqual(`${process.env.E2E_TEST_URL}/accounts`)
+  }
+
+  async navigateToContacts () {
+    await Promise.all([page.waitForNavigation(), page.click('[data-test-id="contacts_nav_btn"]')])
+    expect(page.url()).toEqual(`${process.env.E2E_TEST_URL}/contacts`)
+  }
+
+  async navigateToWallet () {
+    await Promise.all([page.waitForNavigation(), page.click('[data-test-id="wallet_nav_btn"]')])
+    expect(page.url()).toEqual(`${process.env.E2E_TEST_URL}/wallet`)
+  }
+
+  async clickHelpCenter () {
+    let pagesBeforeOpen = await browser.pages()
+    await expect(page).toClick('a', { text: 'Help Center' })
+    var pageCount = 0
+    await runUntilEvaluateEquals(function () {
+      ;(async function () {
+        pageCount = (await browser.pages()).length
+      })()
+      return pageCount
+    }, pagesBeforeOpen.length + 1)
+
+    const browserPages = await browser.pages()
+    const newPopup = browserPages.reduce(function (acc, curr) {
+      if (!pagesBeforeOpen.includes(curr)) {
+        return curr
+      } else {
+        return acc
+      }
+    })
+    expect(newPopup.url()).toEqual(process.env.REACT_APP_FAQ_URL)
+  }
+
+  async clickHelpCenter () {
+    const newPopup = await getNewPopupPage(browser, async () => {
+      await expect(page).toClick('a', { text: 'Help Center' })
+    })
+    expect(newPopup.url()).toEqual(process.env.REACT_APP_FAQ_URL)
+    await newPopup.close()
+  }
+
+  async clickTerms () {
+    const newPopup = await getNewPopupPage(browser, async () => {
+      await expect(page).toClick('a', { text: 'Terms' })
+    })
+    expect(newPopup.url()).toEqual(process.env.REACT_APP_TERMS_URL)
+    await newPopup.close()
+  }
+
+  async checkCopyRight () {
+    const element = await page.$('[data-test-id="copy_right"]')
+    const text = await (await element.getProperty('textContent')).jsonValue()
+    expect(text).toEqual(`© 2018 - ${new Date().getFullYear()} Ventureum Inc.`)
+  }
+
+  async checkBuild () {
+    const element = await page.$('[data-test-id="build_version"]')
+    const text = await (await element.getProperty('textContent')).jsonValue()
+    expect(text).toMatch(/Build/)
+  }
+
+  async checkEmailTransferTitle () {
+    const element = await page.$('[data-test-id="emt_title"]')
+    const text = await (await element.getProperty('textContent')).jsonValue()
+    expect(text).toEqual('Email Transfer')
+  }
+
+  async checkEmailTransferTitleSubtitle () {
+    const element = await page.$('[data-test-id="emt_subtitle"]')
+    const text = await (await element.getProperty('textContent')).jsonValue()
+    expect(text).toEqual('Description goes here...')
+  }
+
+  async checkEmptyImg () {
+    const element = await page.$('[data-test-id="empty_img"]')
+    expect(element.getProperty('src')).toEqual(expect.anything())
+  }
+
+  async checkVideoEmbed () {
+    const element = await page.$('[data-test-id="video_embed"]')
+    const src = await (await element.getProperty('src')).jsonValue()
+    expect(src).toEqual('https://www.youtube.com/embed/TeHbsQ0-wmM')
+  }
+}
+
+export default LandingPage
