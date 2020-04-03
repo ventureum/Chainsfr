15dfa093ce8f1950290fd638f44e54e7780897a5
Timothy Wang <timothywangdev@gmail.com>
2020-04-03 23:40:09 +0000

    Resolve #1430
    Resolve #1430 [Test] Add setAllowance to disconnect.page

--- src/tests/e2e/pages/disconnect.page.js
+++ src/tests/e2e/pages/disconnect.page.js
@@ -1,3 +1,4 @@
+import MetamaskPage from './metamask.page'
 import log from 'loglevel'
 log.setDefaultLevel('info')
 
@@ -6,6 +7,20 @@ class DisconnectPage {
     await expect(page).toClick('button', { text: 'Disconnect' })
     log.info('Disconnect clicked')
     await page.waitForNavigation()
+  }
+
+  async setAllowanceWithMetamask (amount) {
+    const metamaskPage = new MetamaskPage()
+    await expect(page).toFillForm('[data-test-id="set_erc20_allowance"]', {
+        allowance: amount
+      })
+    await expect(page).toClick('button', { text: 'Set Allowance' })
+    await metamaskPage.approve(false)
+    log.info('Sending out tx to reset allowance, wait for confirmation...')
+    await page.waitFor('[data-test-id="allowance_tx_hash"]', {
+      timeout: 180000
+    })
+    log.info('Reset allowance tx confirmed')
   }
 }
 
