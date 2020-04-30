92639daeb3d4b4cedf565fe66fadd1f53b2b7f18
Timothy Wang <timothywangdev@gmail.com>
2020-04-30 19:48:12 +0000

    update

--- src/tests/e2e/jest-environment-puppeteer/custom.setup.js
+++ src/tests/e2e/jest-environment-puppeteer/custom.setup.js
@@ -2,3 +2,33 @@ require('expect-puppeteer')
 global.log = require('loglevel')
 global.log.setDefaultLevel('info')
 jest.setTimeout(900000) // 15 min
+
+jasmine.getEnv().addReporter({
+  specStarted: result => (jasmine.currentTest = result),
+  specDone: result => (jasmine.currentTest = result)
+})
+
+// dump console error logs to stdio
+// make it easier to debug tests
+const chalk = require('chalk')
+page
+  .on('console', message => {
+    const type = message
+      .type()
+      .substr(0, 3)
+      .toUpperCase()
+    const colors = {
+      LOG: text => text,
+      ERR: chalk.red,
+      WAR: chalk.yellow,
+      INF: chalk.cyan
+    }
+    const color = colors[type] || chalk.blue
+    if (type === 'ERR') {
+      console.log(color(`${type} ${message.text()}`))
+    }
+  })
+  .on('pageerror', ({ message }) => console.log(chalk.red(message)))
+  .on('requestfailed', request =>
+    console.log(chalk.magenta(`${request.failure().errorText} ${request.url()}`))
+  )
