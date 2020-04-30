637d7fb0d11d0cfdf0d9b5d1ecaaba41edf7fa32
Timothy Wang <timothywangdev@gmail.com>
2020-04-30 02:34:35 +0000

    Resolve #1389 Setup puppeteer in CI

--- src/tests/e2e/jest.config.js
+++ src/tests/e2e/jest.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   preset: 'jest-puppeteer',
-  testRegex: './*\\.test\\.e2e\\.js$',
   setupTestFrameworkScriptFile: './jest-environment-puppeteer/custom.setup.js',
   globalSetup: './jest-environment-puppeteer/setup.js',
   globalTeardown: './jest-environment-puppeteer/teardown.js',
@@ -8,5 +7,21 @@ module.exports = {
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/mocks/fileMock.js'
-  }
+  },
+  // TODO: more tests will be added
+  testMatch: [
+    '**/emailTransferForm.test.e2e.js',
+    '**/emailTransferFormEntryPoint.test.e2e.js',
+    '**/emailTransferReview.test.e2e.js',
+    '**/directTransferForm.test.e2e.js',
+    '**/directTransferReview.test.e2e.js',
+    '**/accountManagement.test.e2e.js',
+    '**/emailTransferWalletAuth.test.e2e.js',
+    '**/cancelTransfer.test.e2e.js',
+    '**/receiveTransfer.test.e2e.js',
+    '**/landing.test.e2e.js',
+    '**/receipt.test.e2e.js',
+    '**/recipients.test.e2e.js',
+    '**/userSetting.test.e2e.js'
+  ]
 }
